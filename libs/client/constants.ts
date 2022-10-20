b8a0b2ad481d5dfe4f93ca9d903daecf51279b1e
lee <ajaijs1103@naver.com>
2022-10-20 15:45:27 +0900

    chore: DEPLOY_URL 변경

--- libs/client/constants.ts
+++ libs/client/constants.ts
@@ -4,6 +4,6 @@ const BUCKET_URL = "https://seaagainuploads.s3.ap-northeast-2.amazonaws.com/";
 const USE_USER_URL = "/api/users/me";
 const NOTFOUND_URL =
   "https://abs.twimg.com/sticky/illustrations/empty-states/rubber-chicken-400x200.v1.png";
-const DEPLOY_URL = "https://next-twitter-six.vercel.app";
+const DEPLOY_URL = "https://nextjs-twiiter.vercel.app";
 
 export { BLUR_DATA_URL, BUCKET_URL, USE_USER_URL, NOTFOUND_URL, DEPLOY_URL };
