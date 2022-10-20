e1f6431369feabe6a76f614b4a05b05b7d0a06db
lee <ajaijs1103@naver.com>
2022-10-21 01:29:06 +0900

    chore: Image 컴포넌트에 alt 속성 추가

--- components/common/NotFound.tsx
+++ components/common/NotFound.tsx
@@ -6,7 +6,7 @@ function NotFound() {
 	return (
 		<div className='flex flex-col items-center gap-4 mt-10'>
 			<Image src={NOTFOUND_URL} width={300} height={150} blurDataURL={BLUR_DATA_URL}
-				placeholder='blur' />
+				placeholder='blur' alt='NotFound' />
 			<span className='text-2xl font-bold text-center'>Oops... It's Empty</span></div>
 	)
 }
